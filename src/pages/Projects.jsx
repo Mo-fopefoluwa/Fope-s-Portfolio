96012a5f7436195e1f8916b6c6a90b81d505ec4d
Mofopefoluwa <fopefaokunla@gmail.com>
2023-10-23 16:24:54 +0100

    projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -22,29 +22,31 @@ const Projects = () => {
         </motion.h2>
 
         <div className="grid gap-[4rem] sm:grid-cols-2 lg:grid-cols-4">
-          {/* <motion.div variants={fadeIn("up", "tween", 0.4, 1.5)}>
+          <motion.div variants={fadeIn("up", "tween", 0.4, 1.5)}>
             <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="yesapp.jpeg"
+                src="supermarket.jpeg"
                 alt=""
               />
               <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-1 text-lg font-bold text-red-600">Yes App</p>
+                <p className="mb-1 text-lg font-bold text-red-600">
+                  GMarket website
+                </p>
                 <p className="mb-4 text-xs text-gray-100">
                   {" "}
-                  JSX, React JS and Tailwind CSS
+                  Firebase, Firestore, React JS and Tailwind CSS
                 </p>
                 <p className="mb-4 text-xs tracking-wide text-gray-400">
-                  This is a fully functional and responsive sign up and sign in
-                  page . It was built with react js, firebase and styled with
-                  Tailwind css.
+                  A grocery store/ supermarket website with admin page for
+                  adding new items. It was built with react js, firebase and
+                  styled with Tailwind css.
                 </p>
                 <div className="flex items-center justify-center space-x-3">
                   <a
                     target="_blank"
                     rel="noreferrer"
-                    href="https://yes-app.vercel.app/"
+                    href="https://supermarket-template.vercel.app/"
                     className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                   >
                     <i class="bx bx-link-external"></i>
@@ -52,8 +54,40 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </motion.div> */}
+          </motion.div>
+
           <motion.div variants={fadeIn("up", "tween", 0.6, 1.5)}>
+            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img
+                className="object-cover w-full h-56 md:h-64 xl:h-80"
+                src="restaurant.jpeg"
+                alt=""
+              />
+              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p className="mb-1 text-lg font-bold text-red-600">
+                  Restaurant website
+                </p>
+                <p className="mb-4 text-xs text-gray-100">
+                  {" "}
+                  Tailwind CSS, React JS.
+                </p>
+                <p className="mb-4 text-xs tracking-wide text-gray-400">
+                  A simple restaurant website built with Jsx, Tailwind CSS,
+                  React JS
+                </p>
+                <div className="text-white flex items-center justify-center space-x-3">
+                  <a
+                    href="https://restaurantwebsite-template-one.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i class="bx bx-link-external"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div variants={fadeIn("up", "tween", 0.8, 1.5)}>
             <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
@@ -83,38 +117,7 @@ const Projects = () => {
               </div>
             </div>
           </motion.div>
-          {/* <motion.div variants={fadeIn("up", "tween", 0.8, 1.5)}>
-            <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
-              <img
-                className="object-cover w-full h-56 md:h-64 xl:h-80"
-                src="store.jpeg"
-                alt=""
-              />
-              <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-                <p className="mb-1 text-lg font-bold text-red-600">
-                  Online Store
-                </p>
-                <p className="mb-4 text-xs text-gray-100">
-                  {" "}
-                  JSX, Tailwind CSS, React JS, FreeStoreAPI
-                </p>
-                <p className="mb-4 text-xs tracking-wide text-gray-400">
-                  A simple e-commerce store built with Jsx, Tailwind CSS, React
-                  JS and FreeStoreAPI
-                </p>
-                <div className="text-white flex items-center justify-center space-x-3">
-                  <a
-                    href="https://practices-store.vercel.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i class="bx bx-link-external"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </motion.div> */}
-          <motion.div variants={fadeIn("up", "tween", 1.0, 1.5)}>
+          {/* <motion.div variants={fadeIn("up", "tween", 1.0, 1.5)}>
             <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
@@ -144,8 +147,8 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </motion.div>
-          <motion.div variants={fadeIn("up", "tween", 1.2, 1.5)}>
+          </motion.div> */}
+          {/* <motion.div variants={fadeIn("up", "tween", 1.2, 1.5)}>
             <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
@@ -175,8 +178,8 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </motion.div>
-          <motion.div variants={fadeIn("up", "tween", 1.4, 1.5)}>
+          </motion.div> */}
+          {/* <motion.div variants={fadeIn("up", "tween", 1.4, 1.5)}>
             <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
               <img
                 className="object-cover w-full h-56 md:h-64 xl:h-80"
@@ -205,7 +208,7 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </motion.div>
+          </motion.div> */}
         </div>
       </motion.div>
     </div>
